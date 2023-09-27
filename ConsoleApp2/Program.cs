9734bbc55c1b476a5a3d9d9766f91fb3d7e13188
Phoenix <fphoenix923@hotmail.com>
2023-09-28 01:14:44 +0800

    最佳化程式碼

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,11 +2,13 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-//BenchmarkRunner.Run<Test>();
-Console.WriteLine(Test.轉全形文字3("1234567890"));
+BenchmarkRunner.Run<Test>();
+//Console.WriteLine(Test.轉全形文字4("1234567890"));
 
 public class Test
 {
+	private const string InputText = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+
 	public static string 轉全形文字(string input)
 	{
 		char[] c = input.ToCharArray();
@@ -27,29 +29,25 @@ public class Test
 		return new string(c);
 	}
 
-	public static string 轉全形文字2(string input)
+	public static unsafe string 轉全形文字2(string input)
 	{
-		unsafe
+		var a = new string(input);
+
+		fixed (char* c = a)
 		{
-			var a = new string(input);
-
-			fixed (char* c = a)
+			for (int i = 0; i < input.Length; i++)
 			{
-				for (int i = 0; i < input.Length; i++)
+				if (c[i] == 32)
 				{
-					if (c[i] == 32)
-					{
-						c[i] = (char)12288; // Unicode 12288 是「全形空格字元」
-						continue;
-					}
-					else if (c[i] > 32 && c[i] < 127)
-					{
-						c[i] += (char)65248;
-					}
+					c[i] = (char)12288; // Unicode 12288 是「全形空格字元」
 				}
-
-				return a;
+				else if (c[i] > 32 && c[i] < 127)
+				{
+					c[i] += (char)65248;
+				}
 			}
+
+			return a;
 		}
 	}
 
@@ -57,41 +55,65 @@ public class Test
 	{
 		Span<char> c = stackalloc char[input.Length];
 
+		input.AsSpan().CopyTo(c);
+
 		for (int i = 0; i < input.Length; i++)
 		{
 			if (input[i] == 32)
 			{
 				c[i] = (char)12288; // Unicode 12288 是「全形空格字元」
-				continue;
 			}
 			else if (input[i] > 32 && input[i] < 127)
 			{
 				c[i] = (char)(input[i] + 65248);
 			}
-			else
-			{
-				c[i] = input[i];
-			}
 		}
 
-		return c.ToString();
+		return new string(c);
 	}
+
+	public static string 轉全形文字4(string input)
+		=> string.Create(
+			input.Length,
+			input,
+			(dst, src) =>
+			{
+				src.AsSpan().CopyTo(dst);
+
+				for (int i = 0; i < dst.Length; i++)
+				{
+					if (dst[i] == (char)32)
+					{
+						dst[i] = (char)12288; // Unicode 12288 是「全形空格字元」
+					}
+					else if (dst[i] > (char)32 && dst[i] < (char)127)
+					{
+						dst[i] += (char)65248;
+					}
+				}
+			});
 
 	[Benchmark]
 	public void Test1()
 	{
-		轉全形文字("1234567890");
+		轉全形文字(InputText);
 	}
 
 	[Benchmark]
 	public void Test2()
 	{
-		轉全形文字2("1234567890");
+		轉全形文字2(InputText);
 	}
 
 	[Benchmark]
 	public void Test3()
 	{
-		轉全形文字3("1234567890");
+		轉全形文字3(InputText);
+	}
+
+	[Benchmark]
+	public void Test4()
+	{
+		轉全形文字4(InputText);
 	}
 }
